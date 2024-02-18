7e010b61392feea61ed47b12a854ec46edb1ee22
bhaktibuana <bhaktibuana19@gmail.com>
2024-02-18 17:20:07 +0700

    shared: Helpers - create colors constant

---
+++ src/helpers/constants/colors.ts
@@ -0,0 +1,18 @@
+export const defaultColors = {
+  primary: "rgba(0, 150, 120, 1)",
+  white: "rgba(255, 255, 255, 1)",
+  success: "rgba(82, 196, 26, 1)",
+  info: "rgba(22, 119, 255, 1)",
+  warning: "rgba(250, 173, 20, 1)",
+  danger: "rgba(255, 77, 79, 1)",
+  filterBackground: "rgba(0, 0, 0, 0.02)",
+  grey: "rgba(0, 0, 0, 0.04)",
+  borderDefault: "rgba(0, 0, 0, 0.06)",
+  borderInput: "rgba(0, 0, 0, 0.15)",
+  textPlaceholder: "rgba(0, 0, 0, 0.25)",
+  textDescription: "rgba(0, 0, 0, 0.45)",
+  profileName: "rgba(0, 0, 0, 0.65)",
+  backdropDefault: "rgba(0, 0, 0, 0.66)",
+  textDefault: "rgba(0, 0, 0, 0.88)",
+  shadowDefault: "rgba(146, 161, 176, 0.2)",
+};
